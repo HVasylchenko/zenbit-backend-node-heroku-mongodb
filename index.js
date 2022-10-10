d7dd2111b78fa5cba991e81b5e7a8beef20db6da
Hennadii Vasylchenko <vasil@mfdoors.com.ua>
2022-10-10 13:35:36 +0300

    zenbit-backend-mongodb

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import Post from "./Post.js";
+
+const PORT = prosses.env.PORT || 5000;
+const DB_URL = 'mongodb+srv://zenbit:zenbit@cluster0.luqnens.mongodb.net/?retryWrites=true&w=majority'
+// const DB_URL = 'mongodb+srv://zenbit:<password>@cluster0.luqnens.mongodb.net/?retryWrites=true&w=majority'
+
+const app = express();
+app.use(cors({origin:true,credentials: true}));
+app.use(express.json());
+// app.get('/', ((req, res)=>{
+//     res.status(200).json('сервер работает');
+//     // console.log(req.body)
+// }));
+app.post('/', async (req, res)=>{
+    try {
+        const {email, password} = req.body;
+        const post = await Post.create({email, password});
+        // res.status(200).json('сервер работает');
+        res.json(post);
+        // console.log(req.body)
+    } catch (error) {
+        res.status(500).json(error);        
+    }
+});
+
+async function startApp () {
+    try {
+        await mongoose.connect(DB_URL);
+        app.listen(PORT, () => {console.log("SERVER WORKS ON PORT " + PORT)});
+
+    } catch (error) {
+        console.log(error)
+    }
+};
+ startApp(); 
+
+// app.listen(PORT, () => {console.log("SERVER WORKS ON PORT " + PORT)} )
